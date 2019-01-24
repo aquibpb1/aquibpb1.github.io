778ce9358f5060ba0982bf4afe9ebf29fa2f0e53
Aquib <aquib@pebbleroad.com>
2019-01-24 16:36:11 +0800

    append new elements

--- cust-calendar.js
+++ cust-calendar.js
@@ -9642,6 +9642,8 @@ var Calendar = /** @class */ (function () {
                     newView.setElement($("<div class='fc-view fc-" + viewType + "-view' />").appendTo(this.contentEl));
                 }
                 else{
+                    console.log('---first + NEW-----');
+                    //newView.setElement($("<div class='fc-view fc-" + viewType + "-view' />").appendTo(this.contentEl));
                     $('.fc-view-container').find('.fc-list-view tr:last-child').after('<h2>None test</h2>')
                 }
                 
